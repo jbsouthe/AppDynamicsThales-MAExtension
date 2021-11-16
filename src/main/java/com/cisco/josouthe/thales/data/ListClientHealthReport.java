b21c279b7eeecd55180d2dc6c58a78ed0f99c633
jbsouthe <jbsouthe@github.com>
2021-11-16 08:50:24 -0700

    added helper method for reporting

--- src/main/java/com/cisco/josouthe/thales/data/ListClientHealthReport.java
+++ src/main/java/com/cisco/josouthe/thales/data/ListClientHealthReport.java
@@ -1,8 +1,18 @@
 package com.cisco.josouthe.thales.data;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ListClientHealthReport {
     public int skip, limit, total;
     public List<ClientHealthReport> resources;
+    public Map<String,Integer> getCountsByStatus() {
+        Map<String,Integer> map = new HashMap<>();
+        for( ClientHealthReport clientHealthReport : resources ) {
+            int count = map.getOrDefault(clientHealthReport.status, 0)+1;
+            map.put(clientHealthReport.status, count);
+        }
+        return map;
+    }
 }
