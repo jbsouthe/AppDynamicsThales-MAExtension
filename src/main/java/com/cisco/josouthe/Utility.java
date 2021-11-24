5bf2d661f55b415c5ccfa5eea6269ce851043233
jbsouthe <jbsouthe@github.com>
2021-11-24 15:58:23 -0700

    ran into an error where a different time format was in use, hoping this corrects that

--- src/main/java/com/cisco/josouthe/Utility.java
+++ src/main/java/com/cisco/josouthe/Utility.java
@@ -6,13 +6,28 @@ import org.apache.logging.log4j.Logger;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utility {
     private static final Logger logger = LogManager.getFormatterLogger();
-    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+    private static SimpleDateFormat simpleDateFormatWithT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+    private static Pattern patternDateStringWithT = Pattern.compile("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\..*");
+    private static SimpleDateFormat simpleDateFormatWithoutT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z z");
+    private static Pattern patternDateStringWithoutT = Pattern.compile("\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}\\s[+-]\\d{4}\\s[A-Z]{3}");
     private static long dayInMilliseconds = 1000*60*60*24;
 
     public static long getDateFromString( String date ) throws ParseException {
+        if( date == null || "".equals(date) )
+            throw new ParseException("We can't parse an empty date string",-1);
+        SimpleDateFormat simpleDateFormat = null;
+        if(patternDateStringWithT.matcher(date).matches() ) {
+            simpleDateFormat = simpleDateFormatWithT;
+        } else if( patternDateStringWithoutT.matcher(date).matches() ) {
+            simpleDateFormat = simpleDateFormatWithoutT;
+        }
+        if( simpleDateFormat == null )
+            throw new ParseException("We don't have a parser defined for this date format: "+ date, -1);
         return simpleDateFormat.parse(date).getTime();
     }
 
@@ -20,7 +35,7 @@ public class Utility {
 
     public static long gateDaysUntil(String valid_until) {
         try {
-            long expirationTime = simpleDateFormat.parse(valid_until).getTime()-now();
+            long expirationTime = getDateFromString(valid_until)-now();
             return expirationTime/dayInMilliseconds;
         } catch (ParseException e) {
             logger.warn("Parse Exception on date '%s' message: %s",valid_until, e.getMessage());
