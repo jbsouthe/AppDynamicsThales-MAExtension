86515cb4dea611b999374004f822006c659a2768
jbsouthe <jbsouthe@github.com>
2021-11-25 17:02:09 -0700

    Added junit tests to make sure i do not break anything going forward

---
+++ src/main/tests/com/cisco/josouthe/UtilityTest.java
@@ -0,0 +1,31 @@
+package com.cisco.josouthe;
+
+import junit.framework.TestCase;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UtilityTest extends TestCase {
+
+    public void testGetDateFromString() throws ParseException {
+        long timestampOne = Utility.getDateFromString("2021-11-08T10:17:03.000000Z");
+        long timestampTwo = Utility.getDateFromString("2021-11-08 10:17:03 +0000 GMT");
+        System.out.println(timestampOne +" == "+ timestampTwo);
+        assert timestampOne == timestampTwo;
+    }
+
+    public void testNow() {
+        long timestamp = Utility.now();
+        assert timestamp > 0;
+    }
+
+    public void testGetDaysUntil() {
+        SimpleDateFormat simpleDateFormatWithoutT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z z");
+        long timestamp = Utility.now() + (4*24*60*60*1000);
+        String dateString = simpleDateFormatWithoutT.format( new Date(timestamp));
+        long threeDays = Utility.getDaysUntil(dateString);
+        System.out.println("3 days = "+ threeDays);
+        assert threeDays == 3;
+    }
+}
